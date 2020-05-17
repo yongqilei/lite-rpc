569471e54389cd2efb00cff7e2825b64046e4b6b
keanu96 <leiyongqi1026@gmail.com>
2020-05-17 21:55:52 +0900

    Initialize Project: added some modules.

---
+++ lite-rpc-registry/lite-rpc-registry-api/src/main/java/io/lite/rpc/registry/common/Constants.java
@@ -0,0 +1,12 @@
+package io.lite.rpc.registry.common;
+
+public interface Constants {
+
+    String REGISTER = "register";
+
+    String UNREGISTER = "unregister";
+
+    String SUBSCRIBE = "subscribe";
+
+    String UNSUBSCRIBE = "unsubscribe";
+}
